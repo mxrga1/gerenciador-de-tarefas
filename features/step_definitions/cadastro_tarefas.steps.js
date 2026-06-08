e4acd78cc5d861708ea82c05c288a2748913c8cf
mmxrgx <142456639+mmxrgx@users.noreply.github.com>
2026-06-08 20:42:52 -0300

    feat: adiciona step definitions para cadastro de tarefas

---
+++ features/step_definitions/cadastro_tarefas.steps.js
@@ -0,0 +1,38 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const assert = require('assert');
+
+Given('que o usuário está cadastrando uma tarefa', function () {
+  this.tasks = [];
+});
+
+When('ele informa o título {string}', function (titulo) {
+  this.titulo = titulo;
+});
+
+When('salva a tarefa', function () {
+  // ainda não implementado (RED)
+  this.resultado = null;
+});
+
+Then('a tarefa {string} deve ser adicionada à lista', function (titulo) {
+  assert.strictEqual(
+    this.resultado?.title,
+    titulo
+  );
+});
+
+When('ele não informa o título', function () {
+  this.titulo = '';
+});
+
+When('tenta salvar a tarefa', function () {
+  // ainda não implementado (RED)
+  this.resultado = null;
+});
+
+Then('nenhuma tarefa deve ser adicionada à lista', function () {
+  assert.strictEqual(
+    this.resultado?.criada,
+    false
+  );
+});
